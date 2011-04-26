8da251c2e579795a0f19a67d5ac785967b28a6f8
Jrabbit <jackjrabbit+github@gmail.com>
2011-04-26 17:50:07 -0500

    No magic icon!

--- haikunotify.py
+++ haikunotify.py
@@ -7,8 +7,6 @@ if platform.system() != 'Haiku':
     raise OSError
     
 def send_notification(message, title, kind="information", app="PyHaikuNotify", **kwargs):
-    if not icon:
-        icon = "`finddir B_SYSTEM_APPS_DIRECTORY`/AboutSystem"
     options = " --type %s --title %s --app %s" % (kind, title, app)
     for arg in kwargs:
         if arg in ['messageid', 'progress', 'timeout', 'icon', 'onClickApp', 'onClickFile', 'onClickRef','onClickArgv']:
