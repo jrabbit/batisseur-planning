4273de66f2efb0090497ec79463aa2c808e9c3cc
Jrabbit <jackjrabbit+github@gmail.com>
2011-06-10 09:39:42 +0200

    move files

--- achievers/web_achieve.py
+++ achievers/web_achieve.py
@@ -9,19 +9,36 @@ def got_acheivement(username,number):
 @route('/user/:username')
 def userpage(username):
     u = user(username)
-    
+
+@route('/json/:username')
+def show_user(username):
+    return user(username).achievements
+
 def get_db(name="achievers"):
     return anydbm.open(name, 'c')
     
 class user():
+    # Todo use real framework for registration? Or allow users to set a gravatar.
     def __init__(self, name):
         self.db = get_db()
-        self.acheivements = json.loads(self.db[name])
+        if name in self.db:
+            self.acheivements = json.loads(self.db[name])
+        else:
+            self.db[name] = json.dumps(False)
+            self.acheivements = 0
         self.name = name
     def achieve(number):
         self.db[self.name] = json.dumps(json.loads(db[self.name]) + [number])
         self.acheivements = json.loads(self.db[name])
 
+
+
+@route('/js/:filename')
+def js_static(filename):
+    return static_file(filename, root='./js')
+@route('/css/:filename#.+#')
+def css_static(filename):
+    return static_file(filename, root='./css')
 
 if __name__ == '__main__':
     debug(True)
