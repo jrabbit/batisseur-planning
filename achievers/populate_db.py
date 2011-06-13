d65477c8b56b0c086b78f5b9aa425ca4a47b952b
Jrabbit <jackjrabbit+github@gmail.com>
2011-06-13 12:25:30 +0200

    this is better but not working.

--- achievers/populate_db.py
+++ achievers/populate_db.py
@@ -3,10 +3,18 @@ from tempfile import _RandomNameSequence
 
 def makenames():
     randomnames = []
-    for x in _RandomNameSequence():
+    for x in names():
         if len(randomnames) < 7:
             randomnames.append(x) 
     for name in randomnames:
-        urlopen("http://localhost:8080/json/%s" % name)
+        print urlopen("http://localhost:8080/json/%s" % name)
+def names():
+    characters = ("abcdefghijklmnopqrstuvwxyz" +
+                  "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                  "0123456789_")
+    name = ""
+    while len(name) < 5:
+        ''.join(random.choice(characters))
+    yield name
 if __name__ == '__main__':
     makenames()
