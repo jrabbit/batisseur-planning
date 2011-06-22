5fde22f42223eaa12c39413335a8bd6ecae7c7e6
Jrabbit <jackjrabbit+github@gmail.com>
2011-06-22 14:15:33 -0500

    translate varying bep files to each other and json

---
+++ portsutil/transitional.py
@@ -0,0 +1,35 @@
+import json
+
+class New_Bep(object):
+    def __init__(self, bep):
+        self.data = {}
+        for line in bep:
+            if "=" in line:
+                self.data.update([line.split("=")])
+
+class Old_Bep(object):
+    def __init__(self, bep_name):
+        self.data = {}
+        self.bep = file(bep_name).readlines()
+        for i, line in enumerate(self.bep):
+            if "=" in line and "{" not in line:
+                self.simple_line(line)
+            elif '{' in line:
+                self.complex_line(i, line)
+    def simple_line(self, line):
+        self.data.update([line.split("=")])    
+    def complex_line(self, i, line):
+        if '}' in self.bep[i]:
+            self.simple_line(line)
+        else:
+            orig = self.bep[i:self.end_brace(i)]
+            full = ' '.join(orig)
+            k = full.split()[0]
+            l = full[full.find("{"):full.find("}")+1]
+            v = [r.strip() for r in l.split('\n')][1:-1]
+            self.data.update((k,v))
+    def end_brace(self, i):
+        for i2, x in enumerate(self.bep[i:]):
+            if '}' in x:
+                yield i2+i + 1
+                break
