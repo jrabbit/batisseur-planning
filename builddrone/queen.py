b3d4e0dd3df636e31dcd6ab128867c20512db867
Jrabbit <jackjrabbit+github@gmail.com>
2011-06-24 17:57:03 -0500

    handle github service hook

--- builddrone/queen.py
+++ builddrone/queen.py
@@ -1,18 +1,19 @@
 # Recieves post-push information from github/etc and communicates the new versions and blob refs.
 # Upload to Camlistore with git info, this allows users to vouch for it.
 import time
+import json
 from redish.client import Client
-from bottle import route, run, static_file, debug, template, default_app, request
-import camli.op
+from bottle import route, run, static_file, debug, template, default_app, request, post
+# import camli.op
 # TODO: How to share blob refs sanely? [Simple http/json]
-def upload_files(op, path_list):
-  """Uploads a list of files Args:
-    op: The CamliOp to use.
-    path_list: The list of file paths to upload."""
-  real_path_set = set([os.path.abspath(path) for path in path_list])
-  all_blob_files = [open(path, 'rb') for path in real_path_set]
-  logging.debug('Uploading blob paths: %r', real_path_set)
-  op.put_blobs(all_blob_files)
+# def upload_files(op, path_list):
+#   """Uploads a list of files Args:
+#     op: The CamliOp to use.
+#     path_list: The list of file paths to upload."""
+#   real_path_set = set([os.path.abspath(path) for path in path_list])
+#   all_blob_files = [open(path, 'rb') for path in real_path_set]
+#   logging.debug('Uploading blob paths: %r', real_path_set)
+#   op.put_blobs(all_blob_files)
 
 @route('/jobs')
 def availible_jobs(name="All"):
@@ -23,6 +24,20 @@ def availible_jobs(name="All"):
 @route('/pkg/:name')
 def named_pkg(name):
     return availible_jobs(name=name)        
+
+@post('/commit/:pkg')
+def github_hook(pkg):
+    payload = request.forms.get('payload')
+    d = json.loads(payload)
+    new_commit(d['repository']['name'], d)
+
+def new_commit(name, payload):
+    db = get_db()
+    if name not in db.keys():
+        pass
+    else:
+        db['name']['last-commit'] = time.time()
+        db['name']['revs'][payload['commits'][0]['id']] = {'Builds': 0} # Never been built.
 
 def get_db():
     return Client()
@@ -53,3 +68,6 @@ class Job(object):
         #not of the software within
         #Gorram Brecht, we needed that parser for real beps.
     
+
+if __name__ == '__main__':
+    run(host='192.168.1.45', port=8080, reloader=True)
