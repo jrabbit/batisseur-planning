6d9ed1457f327e3e1ec61f4413a00e8e01fb8f47
Jrabbit <jackjrabbit+github@gmail.com>
2011-06-30 22:33:27 -0500

    basic loading of data

--- builddrone/lengtche.py
+++ builddrone/lengtche.py
@@ -7,10 +7,20 @@ class leng_tche(Daemon):
     def run(self):
         while 1:
             #Query the builddrone queen.
-            pass
+            u = urllib.urlopen("http://%s/jobs" % "192.168.1.45") #TODO remove ip
+            data = json.loads(u)
+            
+
+    def do_we_want(self, job):
+        """This will allow users to not compile some dev's packages
+        i.e. Mr. rm -rf"""
+        return True
+
 if __name__ == '__main__':
-    ourdaemon = leng_tche('/boot/home/config/settings/builddrone.pid')
+    ourdaemon = leng_tche('/boot/home/config/settings/Builddrone_pid')
     if sys.argv[1] == "-d":
         ourdaemon.start()
+    elif sys.argv[1] == "-s":
+        ourdaemon.stop()
     else:
         ourdaemon.run()
