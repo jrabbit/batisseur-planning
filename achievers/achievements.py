c2ac8a88ab5a15d21de26061a16af0cd2c6e01b0
Jrabbit <jackjrabbit+github@gmail.com>
2011-07-15 12:19:40 -0600

    new achievement

--- achievers/achievements.py
+++ achievers/achievements.py
@@ -9,7 +9,9 @@ achievements = [{'name': 'Failure', 'desc': 'Had a build fail'},
 {'name': 'Expansionist', 'desc': 'Installed a package'},
 {'name': 'Iron-fisted Ruler', 'desc': 'Forced a build'},
 {'name': 'Curiousity Killed The Cat', 'desc': 'Inquired about a package using -a'},
-{'name': 'Zip Trafficker', 'desc': 'Built a zip for distrobution using -d'}]
+{'name': 'Zip Trafficker', 'desc': 'Built a zip for distrobution using -d'},
+{'name': 'Shredder-9000', 'desc': "I'm sorry I can't let you do that.\n Automated a build with -y"},
+{}]
 
 def get_db(app="haikuports", name="achievements"):
     home = os.path.expanduser('~')
