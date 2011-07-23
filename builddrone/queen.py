41b5d429892d6ee1735d7187742a0d784a2f433d
Jrabbit <jackjrabbit+github@gmail.com>
2011-07-23 16:03:52 -0600

    remove old commented out code

--- builddrone/queen.py
+++ builddrone/queen.py
@@ -6,16 +6,6 @@ import os
 import random
 from redish.client import Client
 from bottle import error, route, run, static_file, debug, template, default_app, request, post
-# import camli.op
-# TODO: How to share blob refs sanely? [Simple http/json]
-# def upload_files(op, path_list):
-#   """Uploads a list of files Args:
-#     op: The CamliOp to use.
-#     path_list: The list of file paths to upload."""
-#   real_path_set = set([os.path.abspath(path) for path in path_list])
-#   all_blob_files = [open(path, 'rb') for path in real_path_set]
-#   logging.debug('Uploading blob paths: %r', real_path_set)
-#   op.put_blobs(all_blob_files)
 
 @route('/jobs')
 def availible_jobs(name="All"):
