b6a89ed0016b2012f95a7c3b3325f36a22889ad2
Jrabbit <jackjrabbit+github@gmail.com>
2011-08-02 19:38:06 -0500

    typo

--- builddrone/test.py
+++ builddrone/test.py
@@ -3,7 +3,7 @@ import uuid
 
 m = leng_tche(None, None) #Avoid Deamonizing
 
-build_info = do_haikuport("joe")
+build_info = m.do_haikuport("joe")
 for l in build_info[0][1].splitlines()[-2:]:
     if l[-4:] == '.zip':
         m.store_zip(l.split()[-1], build_info[1], "joe-"+uuid.uuid4().hex)
