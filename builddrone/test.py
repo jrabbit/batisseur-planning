c8e735f8cc4653ca4eb7908ef4c6080d1584271f
Jrabbit <jackjrabbit+github@gmail.com>
2011-08-02 19:37:11 -0500

    forced testing

---
+++ builddrone/test.py
@@ -0,0 +1,10 @@
+from lengtche import leng_tche
+import uuid
+
+m = leng_tche(None, None) #Avoid Deamonizing
+
+build_info = do_haikuport("joe")
+for l in build_info[0][1].splitlines()[-2:]:
+    if l[-4:] == '.zip':
+        m.store_zip(l.split()[-1], build_info[1], "joe-"+uuid.uuid4().hex)
+
