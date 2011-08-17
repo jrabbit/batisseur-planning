7aeac3c36496e24a9b5f9e15056fc66bc851ac5d
Jrabbit <jackjrabbit+github@gmail.com>
2011-08-17 15:15:57 -0500

    fix the models

--- builddrone/model.py
+++ builddrone/model.py
@@ -1,8 +1,11 @@
-from elixir import metadata, using_options, Entity, Field
+from elixir import metadata, options_defaults, Entity, Field
 from elixir import Integer, Unicode, UnicodeText, Numeric
+from elixir import setup_all, OneToMany, ManyToOne
+import sqlalchemy
 
 metadata.bind = "sqlite:///builddrone.sqlite"
-metadata.bind.echo = True #TODO Disable.
+metadata.bind.echo = True #TODO Disable. Debugs all SQL ops
+options_defaults['session'] =  sqlalchemy.orm.scoped_session(sqlalchemy.orm.sessionmaker(autocommit=True,autoflush=True))
 
 class JobId(Entity):
     arch = Field(UnicodeText)
@@ -11,15 +14,24 @@ class JobId(Entity):
     uuid = Field(UnicodeText, primary_key=True)
     blob = Field(UnicodeText)
     status = Field(UnicodeText)
+    @property
+    def jobid(self):
+        return self.name +'-'+ self.arch +'-'+ self.gcc +'-'+ self.uuid
 
 class Project(Entity):
     vcs = Field(UnicodeText)
     name = Field(UnicodeText, primary_key=True)
-
-class Commit(Entity):
-    project = Field(UnicodeText)
-    branch = Field(UnicodeText)
+    branches = OneToMany('Branch')
+    owner = Field(UnicodeText)
+    
+class Branch(Entity):
+    project = ManyToOne('Project')
+    commits = OneToMany('Commit')
+class Commit(Branch):
+    branch = ManyToOne('Branch')
     sha = Field(UnicodeText, primary_key=True)
     time = Field(Numeric)
     builds = Field(Numeric, default=0)
-    
+
+if __name__ == '__main__':
+    setup_all()
